6dcae2f7deaeb9f84586eca996579139cdc550c8
Surya Narayan M S <suryanarayan.1905147@srec.ac.in>
2022-03-28 09:34:51 +0530

    app name changed

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Butterfly Effect Admin',
+      title: 'Tamil Meme Templates Admin',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
